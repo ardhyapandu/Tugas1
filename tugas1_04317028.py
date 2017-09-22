111df1556c910951f14431e7673ab594056d1e81
Ardhya Pandu <32034853+ardhyapandu@users.noreply.github.com>
2017-09-22 09:24:18 +0700

    Add files via upload
    tugas1 selesai

---
+++ tugas1_04317028.py
@@ -0,0 +1,10 @@
+nama = "Ardhya Pandu Pratama"
+nim = "04317028"
+umur = 19
+
+print ("Nama = ", nama)
+print ("NIM = ", nim)
+print ("Umur = ")
+print (umur)
+print ("Umur 5 Tahun Kemudian = ")
+print (umur+5)
